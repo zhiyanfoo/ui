e9c71b881f2e416d461b6fc9473d04cd0d321db2
Zhiyan Foo <zhiyanfoo@gmail.com>
2019-03-31 19:19:37 -0400

    Tada

--- src/components/messages-list.tsx
+++ src/components/messages-list.tsx
@@ -1,15 +1,12 @@
 import * as React from 'react'
 import Message from './message'
-import {Message as MessageType} from '../types'
+import { Message as MessageType } from '../types'
 import * as hash from 'object-hash'
 
-export default ({messages }: {messages: MessageType[]}) => {
-  console.log('in message list')
-  console.log(messages)
-  console.log(messages.map)
+export default ({ messages }: { messages: MessageType[] }) => {
   return (
     <section id="messages-list">
-      {messages.map((message) => (
+      {messages.map(message => (
         <Message key={hash(message)} {...message} />
       ))}
     </section>
--- src/containers/messages-list.tsx
+++ src/containers/messages-list.tsx
@@ -3,9 +3,6 @@ import MessagesListComponent from '../components/messages-list'
 import { State } from '../types'
 
 const mapStateToProps = (state: State) => {
-  console.log('state')
-  console.log(state)
-  console.log(state.messages)
   return state.messages
 }
 
